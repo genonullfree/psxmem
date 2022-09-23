cc6a3bc19425e0e1ee6cc3b0f1ada358dc47c748
geno <nullfree.geno@gmail.com>
2022-09-22 23:29:54 -0400

    Make better

--- src/lib.rs
+++ src/lib.rs
@@ -523,38 +523,18 @@ impl MemCard {
     }
 
     pub fn find_game(&self, search: &str) -> Result<Vec<DataBlock>, MCError> {
-        let mut idxs = Vec::<usize>::new();
         let mut found = Vec::<DataBlock>::new();
         let mut needle = String::from(search);
         needle.make_ascii_lowercase();
 
-        // Find names that match in the info block
-        for (num, info) in self.info.dir_frames.iter().enumerate() {
-            let i = info.get_region_info()?;
-            let mut haystack = String::from(&i.name);
-            haystack.make_ascii_lowercase();
-
-            if haystack.contains(&needle) {
-                if !idxs.contains(&num) {
-                    idxs.push(num);
-                }
-            }
-        }
-
         // Find names that match in the data blocks
-        for (num, info) in self.data.iter().enumerate() {
-            let mut haystack = String::from(info.title_frame.decode_title()?);
+        for info in &self.data {
+            let mut haystack = info.title_frame.decode_title()?;
             haystack.make_ascii_lowercase();
 
             if haystack.contains(&needle) {
-                if !idxs.contains(&num) {
-                    idxs.push(num);
-                }
+                found.push(info.clone());
             }
-        }
-
-        for i in idxs {
-            found.push(self.data[i].clone());
         }
 
         Ok(found)
